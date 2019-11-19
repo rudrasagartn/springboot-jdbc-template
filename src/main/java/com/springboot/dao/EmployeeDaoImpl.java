006c860800d7c1d5c7d3a1666999212122b9c78f
rudrasagartn <rudrasagar.tn@VDC01DLD2520.IN.valtech.com>
2019-11-19 15:55:44 +0530

    organized imports

--- src/main/java/com/springboot/dao/EmployeeDaoImpl.java
+++ src/main/java/com/springboot/dao/EmployeeDaoImpl.java
@@ -3,7 +3,6 @@ package com.springboot.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
